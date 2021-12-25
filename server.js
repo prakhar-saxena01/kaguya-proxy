20d1cf32ed0f322e8e25f11b33a94679252c7364
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-12-25 17:36:10 +0700

    changes

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const qs = require("qs");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get("/", async (req, res) => {
+app.get("/secret", async (req, res) => {
   const { url, headers } = req.query;
 
   const decodedUrl = decodeURIComponent(url);
@@ -18,9 +19,23 @@ app.get("/", async (req, res) => {
     responseType: "stream",
     headers: { ...req.headers, ...headers, host },
     validateStatus: () => true,
+    maxRedirects: 0,
   });
 
+  res.setHeader("Cache-Control", "public, max-age=3600");
+
   for (let header in response.headers) {
+    if (header.toLowerCase() === "location") {
+      res.redirect(
+        302,
+        `/secret?url=${encodeURIComponent(
+          response.headers[header]
+        )}&${qs.stringify({ headers })}`
+      );
+
+      return;
+    }
+
     res.setHeader(header, response.headers[header]);
   }
 
